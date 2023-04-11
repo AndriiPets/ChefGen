8ac84676a435e0da663bffbce5fe688b003cc84b
AndriiPets <nnggtrgftgd@gmail.com>
2023-04-11 23:46:30 +0300

    initial

---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export interface Home {}
+
+function Home() {
+  return (
+    <div className="flex">
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+      <h1>HELLO</h1>
+    </div>
+  );
+}
+
+export default Home;
